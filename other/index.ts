7439c4c14adda6ed6df5686a4a8622b136e1a03d
james-cain <m15960192140@163.com>
2018-10-29 23:16:11 +0800

    feat: ts别的总结

---
+++ other/index.ts
@@ -0,0 +1,13 @@
+// never https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Basic%20Types.html#never
+
+// 在泛型里使用类类型 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Generics.html#%E5%9C%A8%E6%B3%9B%E5%9E%8B%E9%87%8C%E4%BD%BF%E7%94%A8%E7%B1%BB%E7%B1%BB%E5%9E%8B
+
+// 数组的类型推论 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Type%20Inference.html#%E6%9C%80%E4%BD%B3%E9%80%9A%E7%94%A8%E7%B1%BB%E5%9E%8B
+
+// 函数输入的类型推论 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Type%20Inference.html#%E4%B8%8A%E4%B8%8B%E6%96%87%E7%B1%BB%E5%9E%8B
+
+// 允许不严格符合类型，只需要在一定规则下兼容即可 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Type%20Compatibility.html
+
+// 使用 & 将多种类型的共有部分叠加成一种类型 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Advanced%20Types.html#%E4%BA%A4%E5%8F%89%E7%B1%BB%E5%9E%8B%EF%BC%88intersection-types%EF%BC%89
+
+// 混入 https://zhongsp.gitbooks.io/typescript-handbook/content/doc/handbook/Mixins.html
